b8557dac15d7a1e872cca2b398a16218071033a3
Jatinder Singh <jatinder.1064@gmail.com>
2022-07-18 12:51:56 -0400

    Add files via upload

---
+++ cloud-api-gateway/src/main/java/com/jatinder/cloudapigateway/controller/FallbackMethodsController.java
@@ -0,0 +1,20 @@
+package com.jatinder.cloudapigateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackMethodsController {
+
+    @GetMapping("/userServiceFallback")
+    public String userServiceFallbackMethod() {
+        return "User Service is taking longer than expected." +
+                " Please try again later...";
+    }
+
+    @GetMapping("/departmentServiceFallback")
+    public String departmentServiceFallbackMethod() {
+        return "Department Service is taking longer than expected." +
+                " Please try again later...";
+    }
+}
